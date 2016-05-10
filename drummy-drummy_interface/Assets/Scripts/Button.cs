24866da480b4e135c98b4875d2c41655d78c0b5c
Dan Coppen <kimera@hotmail.co.uk>
2016-05-10 18:04:17 +0900

    Matrix of buttons and background
    Assigned IDs to 16 buttons

---
+++ drummy-drummy_interface/Assets/Scripts/Button.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour {
+
+	public Sprite pushedSprite;
+	public Sprite unpushedSprite;
+
+	public int id;
+
+	void OnMouseDown()
+	{
+		SetState (true);
+		Debug.Log("pushed");
+		Debug.Log (id);
+	}
+
+	void OnMouseUp()
+	{
+		SetState (false);
+	}
+
+	void SetState(bool state)
+	{
+		Sprite sprite;
+
+		if (state) {
+			sprite = pushedSprite;
+		} else {
+			sprite = unpushedSprite;
+		}
+
+		GetComponent<SpriteRenderer> ().sprite = sprite;
+	}
+}
+
